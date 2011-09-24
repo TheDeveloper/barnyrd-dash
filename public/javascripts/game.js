fa523bea8b1087e380979964cb40c4fb4606ce0e
Max Williams <max@pusher.com>
2011-09-24 15:35:26 -0700

    Stuff

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -1,8 +1,5 @@
-var sprites = [];
-var playerLength = 7;
-for (var i=0; i < playerLength; i++) {
-	sprites.push( new Sprite(i) )
-};
+
+
 var showNotice = function(msg){
 	$('.notice').remove()
 	var el = $('<div class="notice">'+ msg + '</div>')
@@ -20,27 +17,46 @@ var PlayView = function(sprites){
 	var spriteWidth = 30;
 	var margin = (laneHeight - spriteHeight) / 2
 	
-	this.render = function(){
+	this.render = function(frameNum){
 		ctx.clearRect(0,0, canvas.width, canvas.height)
 		for (var i=0; i < sprites.length; i++) {
-			// ctx.save()
-			// ctx.transform()
+			ctx.save()
+			ctx.translate(
+				sprites[i].position.x + (spriteWidth/2),
+				((laneHeight * sprites[i].lane) + (laneHeight/2) + sprites[i].position.y) 
+			);
+			ctx.rotate(Math.sin(frameNum) * 0.3)
 			ctx.drawImage(
 				imgSprite, 
-				sprites[i].position.x,
-				(laneHeight * sprites[i].lane) + margin + sprites[i].position.y,
+				-spriteWidth/2,
+				-spriteHeight/2,
 				spriteHeight,
 				spriteWidth
-			)
+			);
+			ctx.restore();
 		};
 	}
 }
 
 var Game = function(sprites){
-	var playView = new PlayView(sprites);
 	var gameInterval;
 	var self = this
 	var frameNum;
+	var sprites = [];
+	var playerLength = 7;
+	for (var i=0; i < playerLength; i++) {
+		sprites.push( new Sprite(i) )
+	};
+	var playView = new PlayView(sprites);
+	
+	var finish = function(){
+		showNotice('<p>we have a winner!</p>')
+		var btn = $('<a href="#">Play again</a>').click(function(){
+			new Game()
+		});
+		$('.notice').append(btn)
+		self.stop()
+	}
 	
 	this.stop = function(){
 		clearInterval(gameInterval)
@@ -54,22 +70,23 @@ var Game = function(sprites){
 			for (var i=0; i < sprites.length; i++) {
 				sprites[i].onEnterFrame(frameNum);
 				if (sprites[i].position.x > 700){
-					showNotice('we have a winner!')
-					self.stop()
+					finish();
 				}
 			};
 			
-			playView.render();
+			playView.render(frameNum);
 		}, 50)
 	}
+	
+	self.start();
 }
 
 $().ready(function(){
 	var raceScreen = new RaceScreen();
 	var lobbyScreen = new LobbyScreen(function(){
 		$('.gameArea').html( raceScreen.template )
-		var game = new Game(sprites)
-		game.start();
+		new Game()
+		return false;
 	})
 	$('.gameArea').html( lobbyScreen.template )
 });
