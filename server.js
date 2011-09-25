8159e4c436f913b578b9ee42b6049dd18a318998
Nuno Job <nunojobpinto@gmail.com>
2011-09-24 21:49:57 -0700

    [server] changes config files
    * added betable support
    * commited some legacy code i had on the server

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express    = require('express')
-  , cfg        = require('./conf/config')
+  , cfg        = require('./conf/config').www
   , handler    = require('./handler')
   , app        = express.createServer()
   ;
@@ -7,10 +7,11 @@ var express    = require('express')
 app.configure( function(){
   app.use(express.bodyParser());
   app.use(app.router);
+  app.use(express.cookieParser());
+  app.use(express.session({ secret: cfg.secret }));
   app.use(express.static(__dirname + '/public'));
 });
 
-// request.body for 
 app.get('/', function(req, res){
   res.sendfile('public/splash.html');
 });
@@ -26,10 +27,10 @@ app.get('/accountInfo', function(req, res){
   res.send(JSON.stringify(jsonRes));
 });
 
-app.listen(cfg.www.port, function(err) {
+app.listen(cfg.port, function(err) {
   if (err) { throw err; }
   console.log( '{"www": "ok", "host": "%s", "port": "%d", "env": "%s"}', 
-    cfg.www.host, cfg.www.port, app.settings.env);
+    cfg.host, cfg.port, app.settings.env);
 });
 
 process.on('uncaughtException', function(err) {
