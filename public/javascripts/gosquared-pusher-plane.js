86fbd7279410f2f6fee286a601aed3542318c44b
Geoff Wagstaff <geoff@gosquared.com>
2011-09-24 17:29:44 -0700

    Awesome plane w/banner

---
+++ public/javascripts/gosquared-pusher-plane.js
@@ -0,0 +1,16 @@
+function doPlane(){
+  var plane = $('#plane');
+  if(!plane.length){
+    plane = $('<div/>', 
+      {
+        id: 'plane'
+      , css : {
+          left: $(window).width()
+      }
+    });
+    plane.prependTo($('body'));
+  }
+  plane.css({left: $(window).width()});
+  plane.animate({left: -1*$(plane).width()}, 10000, 'linear', function(){setTimeout(doPlane, 5000)});
+}
+$(document).ready(doPlane);
